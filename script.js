f7b00f932d158816516cd27bf141086bccb74184
Mark Moskalevskij <dohaa@ukr.net>
2023-02-19 01:50:05 +0200

    add homework 3

---
+++ script.js
@@ -0,0 +1,9 @@
+let a = prompt('Как тебя зовут?');
+let b = prompt('Сколько тебе лет?');
+let c = prompt('В какой стране ты живешь?');
+alert(a.concat(' ', b, ' ', c));
+
+let number = 12345;
+array = ('' + number).split('')
+console.log(array.map(Number));
+alert(array.map(Number))
