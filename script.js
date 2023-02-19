4ca0307774301ecab3d04b8f9371f6481ab8040a
Mark Moskalevskij <dohaa@ukr.net>
2023-02-19 20:32:35 +0200

    correction homework 3

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let c = prompt('В какой стране ты живешь?');
 alert(a.concat(' ', b, ' ', c));
 
 let number = 12345;
-array = ('' + number).split('')
-console.log(array.map(Number));
-alert(array.map(Number))
+array = number.toString();
+let regexp = /\d/g;
+alert( array.match(regexp).join(' '));
+alert( array.split("").reverse().join(" "));
